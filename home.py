15a0f8923eba9e556d9a4d2c03682c9e26a9ffb5
Chris Lenart <clenart4@kent.edu>
2019-04-29 21:00:13 -0400

    Proper saving

--- home.py
+++ home.py
@@ -169,17 +169,17 @@ def pick():
 		guest_list = []
 		
 		if (len(rsvp_code) > 0):
-			rsvp_code = rsvp_code[0]["code"]
-			user_list = db.search((query_db.code == rsvp_code) & (query_db.email != user))
+			rsvp_code2 = rsvp_code[0]["code"]
+			user_list = db.search((query_db.code == rsvp_code2) & (query_db.email != user))
 			if request.method == 'POST':
 				list = []
 				for person in request.form['list'].split(","):
-					email = db.search((query_db.code == rsvp_code) & (query_db.name == person))
+					email = db.search((query_db.code == rsvp_code2) & (query_db.name == person))
 					list.append(email[0]["email"])
 				db.update({'hateList': list}, query_db.email == user)
-				flash("You have updated your seating preference.", "success")
+				flash("You have updated your seating preference. Please reference to see reflected values.", "success")
 			for guest in user_list:
-				guest_list.append(guest["name"])
+				guest_list.append([guest["name"], True if guest["email"] in rsvp_code[0]["hateList"] else False])
 		else:
 			flash("Invalid usercode: Please contact web-admins for new code.", "danger")
 		return render_template('preferred.html', guests=guest_list)
