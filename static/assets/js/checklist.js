84f2bc98c6e111adf329a26d4688bbcd31129097
Chris Lenart <clenart4@kent.edu>
2019-05-01 00:06:15 -0400

    homepage update + design timeline

--- static/assets/js/checklist.js
+++ static/assets/js/checklist.js
@@ -1,3 +1,10 @@
 $("li").click(function() {
- $(this).toggleClass("checked");
+	 $(this).toggleClass("checked");
+	 if ($(this).parent().children().length == $(this).parent().children(".checked").length) { // all are checked
+		$(this).parent().parent()[0].children[0].src = "/static/assets/img/Platter Closed.png";
+		$(this).parent().parent()[0].children[1].style.visibility = "hidden";
+	 } else {
+		$(this).parent().parent()[0].children[0].src = "/static/assets/img/Platter Open.png";
+		$(this).parent().parent()[0].children[1].style.visibility = "visible";
+	 }
 });
